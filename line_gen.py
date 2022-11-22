187c500348dd17089b9b2c68fc8110b127dc2c84
Sofia <shamby@bowdoin.edu>
2022-11-22 02:35:28 -0500

    more diccy progress

--- line_gen.py
+++ line_gen.py
@@ -25,7 +25,8 @@ n_grams = {}
 
 for sentence in sentences:
     #remove nonn char
-    words = [word for word in sentence if word[0].isalpha()]
+    words = [word for word in sentence \
+        if (word[0].isalpha() and word[0].lower())]
     
     for i in range(len(words) - 1):
         #every ending word ignored (bc no list of words after)
@@ -55,7 +56,7 @@ def generate_sentence(nb = 6): # should this be random length
 
     output_line = " ".join(words)
     
-    return output_line
+    return output_line.lower()
 
 def pull_nouns_and_adjs(output_line):
     """takes out nouns from our sentence"""
@@ -66,15 +67,16 @@ def pull_nouns_and_adjs(output_line):
     
     return nouns_adjs
 
-print("sentence here")
 n_gram_sent = generate_sentence()
-print("n_gram_sent:" + n_gram_sent)
+print("n_gram_sent: ") 
+print(n_gram_sent)
 
 pulled_nouns_and_adjs = pull_nouns_and_adjs(n_gram_sent)
 print(pulled_nouns_and_adjs)
 
 #should run nouns and adjectives into trex 
 colors = ['pink', 'red', 'green']
+
 def determine_emotions(nouns_adjs):
     # looks at main emotions associated with adjectives
     
@@ -87,36 +89,35 @@ def determine_emotions(nouns_adjs):
 
     return to_be_converted 
 
+
 def convert_to_dict(to_be_converted):
+    
     cute_dict = {}
     words = to_be_converted
     i = 0
+
+    empty_val = [('fear', 0.0), ('anger', 0.0), ('anticip', 0.0), \
+        ('trust', 0.0), ('surprise', 0.0), ('positive', 0.0), \
+        ('negative', 0.0), ('sadness', 0.0), ('disgust', 0.0), ('joy', 0.0)]
     
     while (i < len(words)):
+        
         cute_dict[words[i][0]] = words[i][2]
         i += 1
+    
+    cute_dict = {key:val for key, val in cute_dict.items() if val != empty_val}
     return cute_dict
     
 
-
-def cleanup_emotions(emotion_dict):
-    pass
-
-#print(determine_emotions(adj_sent, pulled_nouns_r
-#))
 emotional_words = determine_emotions(pulled_nouns_and_adjs)
-print(emotional_words)
-print("indexing attempts")
-print(emotional_words[0][2]) # this gives us list
-print(emotional_words[0][0]) # gives us word
-print(emotional_words[1][0])
-print(len(emotional_words))
 print("dictionary******")
-print(convert_to_dict(emotional_words))
+cute_emotional_dict = convert_to_dict(emotional_words)
+print(cute_emotional_dict)
+
+
 
 # if you can make this into a dict, then can put keys into trex - maybe split left of :
     # once you have list of values from trex
-    # run determine_emotions 
     # pull out keys that actually have values
     # use word to create newspeak buncha if statements
     # rerun can just involve user selecting the category
